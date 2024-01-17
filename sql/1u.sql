5d0decbf6a66734e51ebaf2f94890fe3a9dc757c
myjhye <t01087633799@gmail.com>
2024-01-18 01:42:58 +0900

    테이블 수정

--- sql/1u.sql
+++ sql/1u.sql
@@ -22,7 +22,8 @@ create table if not exists public.follows (
     user_id bigint not null references public.users (id),
     followers_id bigint not null references public.users (id),
     created_at timestamp default now(),
-    updated_at timestamp default now()
+    updated_at timestamp default now(),
+    unique(user_id, followers_id)
 ); 
 
 create index posts_user_id_index on public.posts (user_id);
